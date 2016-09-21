64e69e942a7006aedf5b242980eec95eacde8430
Francisco Nogueira Calmon Sobral <fsobral@uem.br>
2016-09-21 20:35:57 -0300

    Changed working days by full days when computing taxes.

--- pyncomes/venda_compra.py
+++ pyncomes/venda_compra.py
@@ -22,21 +22,25 @@ def vc_alpha(alpha, V, i1, i2, t0, d, tf):
 
     hoje = datetime.date.today()
 
-    Nhoje = workdays.networkdays(t0, hoje)
+    Nhoje = (hoje - t0).days
 
     N = workdays.networkdays(t0, tf)
+    Nc = (tf - t0).days
 
     proxInv = hoje + datetime.timedelta(days=d)
 
     Nprox = workdays.networkdays(proxInv, tf)
+    Nproxc = (tf - proxInv).days
 
-    print N, Nhoje, Nprox
+    print "\nN: {0:4d}, N Corr: {1:4d}, N Hoje: {2:4d}, " \
+        "N Prox: {3:4d}, N Prox Corr: {4:4d}\n".format(
+            N, Nc, Nhoje, Nprox, Nproxc)
 
     i1d = math.pow(1.0 + i1, 1.0 / util.DIASANO) - 1.0
     i2d = math.pow(1.0 + i2, 1.0 / util.DIASANO) - 1.0
 
-    t1 = util.ir(N) + (1.0 - util.ir(N)) * math.pow(1.0 + i1d, N)
-    t2 = util.ir(Nprox) + (1.0 - util.ir(Nprox)) * math.pow(1.0 + i2d, Nprox)
+    t1 = util.ir(Nc) + (1.0 - util.ir(Nc)) * math.pow(1.0 + i1d, N)
+    t2 = util.ir(Nproxc) + (1.0 - util.ir(Nproxc)) * math.pow(1.0 + i2d, Nprox)
 
     vLim = (alpha * t1 / (t2 * (1.0 - util.ir(Nhoje))) -
             util.ir(Nhoje) / (1.0 - util.ir(Nhoje))) * V
